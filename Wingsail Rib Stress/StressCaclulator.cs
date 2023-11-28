91a42cc22cefdfd41c8795ecf9cf91b7663dd012
SpencerHryciw <Spencer@DESKTOP-1V49N64>
2023-11-27 18:04:49 -0800

    updated 90 degree loading forces

--- Wingsail Rib Stress/StressCaclulator.cs
+++ Wingsail Rib Stress/StressCaclulator.cs
@@ -38,7 +38,7 @@ namespace Wingsail_Rib_Stress
         static PolynomialCalculus trailingEdge = new("-1 1 1"); //m
 
         //the locations of the ribs where 0 is the bottom of the wing, and 1 is the top
-        static double[] ribLocations = new double[] { 0, 0.5, 1 };
+        static double[] ribLocations = new double[] { 0, 1.0 / 6.0, 1.0 / 3.0, 0.5, 2.0 / 3.0, 5.0 / 6.0, 1 };
 
         public static void Main()
         {
@@ -46,22 +46,11 @@ namespace Wingsail_Rib_Stress
             PolynomialCalculus chordLength = leadingEdge.Clone();
             chordLength.AddPolynomial(trailingEdge);
 
-            //initialize the apparent width
-            PolynomialCalculus apparentWidth = chordLength.Clone();
-            apparentWidth.MultiplyByConstant(airfoilWidthCoefficient);
-
             //--------------------------------------//
             //0 degrees angle of attack calculations//
             //--------------------------------------//
 
-            //drag force = Cd * density * 1/2 * V^2 * area -> drag pressure = drag force / area
-            double dragPressure = dragCoefficient0Deg * airDensity * 0.5 * windSpeed * windSpeed;
-
-            //get the drag force per length for a given location on the wing
-            PolynomialCalculus dragForce = apparentWidth.Clone();
-            dragForce.MultiplyByConstant(dragPressure); //N / m
-
-
+            //im so lost in the sauce
 
             //---------------------------------------//
             //90 degrees angle of attack calculations//
@@ -70,41 +59,40 @@ namespace Wingsail_Rib_Stress
             //dynamic pressure = 1/2 * rho * V^2
             double dynamicPressure = 0.5 * airDensity * windSpeed * windSpeed;
 
-            //y is inward force, x is shear along the mast axis, L is chord length, P is pressure
-            //dFy = P * L * dx
-            //dFx = dL/dx * dFy
+            //F_in is inward force, F_shear is shear along the mast axis, L is chord length, P is pressure, c is the NACA width coefficicent
+            //dF_in = P * L(x) * dx
+            //dF_shear = P * c/2 * dL/dx * L(x) * dx
 
             double[] inwardForce90Deg = new double[ribLocations.Length];
             double[] shearForce90Deg = new double[ribLocations.Length];
 
-            //do the side facing the wind first
             for (int i = 0; i < ribLocations.Length; i++)
             {
                 if (i == 0)
                 {
-                    inwardForce90Deg[0] = dynamicPressure * chordLength.EvaluatePolynomialIntegral(0, wingHeight * (ribLocations[1] + ribLocations[0]) / 2);
-                    shearForce90Deg[0] = dynamicPressure * airfoilWidthCoefficient * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight * (ribLocations[1] + ribLocations[0]) / 2) * chordLength.EvaluatePolynomial(wingHeight * (ribLocations[1] + ribLocations[0]) / 2)) - (chordLength.EvaluatePolynomial(0) * chordLength.EvaluatePolynomial(0)));
+                    //a = 0
+                    //b = wingHeight * 0.5 * ribLocations[i + 1]
+
+                    inwardForce90Deg[i] = dynamicPressure * chordLength.EvaluatePolynomialIntegral(0, wingHeight * 0.5 * ribLocations[i + 1]);
+                    shearForce90Deg[i] = airfoilWidthCoefficient * dynamicPressure * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight * 0.5 * ribLocations[i + 1]) * chordLength.EvaluatePolynomial(wingHeight * 0.5 * ribLocations[i + 1])) - (chordLength.EvaluatePolynomial(0) * chordLength.EvaluatePolynomial(0)));
                 }
                 else if (i == ribLocations.Length - 1)
                 {
-                    inwardForce90Deg[i] = dynamicPressure * chordLength.EvaluatePolynomialIntegral(wingHeight * (ribLocations[i] + ribLocations[i - 1]) / 2, wingHeight);
-                    shearForce90Deg[i] = dynamicPressure * airfoilWidthCoefficient * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight) * chordLength.EvaluatePolynomial(wingHeight)) - (chordLength.EvaluatePolynomial(wingHeight * (ribLocations[i] + ribLocations[i - 1]) / 2) * chordLength.EvaluatePolynomial(wingHeight * (ribLocations[i] + ribLocations[i - 1]) / 2)));
+                    //a = wingHeight * 0.5 * (1 + ribLocations[i - 1])
+                    //b = wingHeight
+
+                    inwardForce90Deg[i] = dynamicPressure * chordLength.EvaluatePolynomialIntegral(wingHeight * 0.5 * (1 + ribLocations[i - 1]), wingHeight);
+                    shearForce90Deg[i] = airfoilWidthCoefficient * dynamicPressure * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight) * chordLength.EvaluatePolynomial(wingHeight)) - (chordLength.EvaluatePolynomial(wingHeight * 0.5 * (1 + ribLocations[i - 1])) * chordLength.EvaluatePolynomial(wingHeight * 0.5 * (1 + ribLocations[i - 1]))));
                 }
                 else
                 {
-                    inwardForce90Deg[i] = dynamicPressure * chordLength.EvaluatePolynomialIntegral(wingHeight * (ribLocations[i] + ribLocations[i - 1]) / 2, wingHeight * (ribLocations[i] + ribLocations[i + 1]) / 2);
-                    shearForce90Deg[i] = dynamicPressure * airfoilWidthCoefficient * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight * (ribLocations[i] + ribLocations[i + 1]) / 2) * chordLength.EvaluatePolynomial(wingHeight * (ribLocations[i] + ribLocations[i + 1]) / 2)) - (chordLength.EvaluatePolynomial(wingHeight * (ribLocations[i] + ribLocations[i - 1]) / 2) * chordLength.EvaluatePolynomial(wingHeight * (ribLocations[i] + ribLocations[i - 1]) / 2)));
+                    //a = wingHeight * 0.5 * (ribLocations[i] + ribLocations[i - 1])
+                    //b = wingHeight * 0.5 * (ribLocations[i] + ribLocations[i + 1])
+
+                    inwardForce90Deg[i] = dynamicPressure * chordLength.EvaluatePolynomialIntegral(wingHeight * 0.5 * (ribLocations[i] + ribLocations[i - 1]), wingHeight * 0.5 * (ribLocations[i] + ribLocations[i + 1]));
+                    shearForce90Deg[i] = airfoilWidthCoefficient * dynamicPressure * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight * 0.5 * (ribLocations[i] + ribLocations[i + 1])) * chordLength.EvaluatePolynomial(wingHeight * 0.5 * (ribLocations[i] + ribLocations[i + 1]))) - (chordLength.EvaluatePolynomial(wingHeight * 0.5 * (ribLocations[i] + ribLocations[i - 1])) * chordLength.EvaluatePolynomial(wingHeight * 0.5 * (ribLocations[i] + ribLocations[i - 1]))));
                 }
             }
-
-            double totalInwardForce90Deg = dynamicPressure * chordLength.EvaluatePolynomialIntegral(0, wingHeight);
-            double totalShearForce90Deg = dynamicPressure * airfoilWidthCoefficient * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight) * chordLength.EvaluatePolynomial(wingHeight)) - (chordLength.EvaluatePolynomial(0) * chordLength.EvaluatePolynomial(0)));
-
-            //do the side facing away from the wind second
-            //inwardForce90Deg += airPressure * chordLength.EvaluatePolynomialIntegral(0, wingHeight);
-            //shearForce90Deg += airPressure * airfoilWidthCoefficient * 0.5 * ((chordLength.EvaluatePolynomial(wingHeight) * chordLength.EvaluatePolynomial(wingHeight)) - (chordLength.EvaluatePolynomial(0) * chordLength.EvaluatePolynomial(0)));
-
-
         }
     }
 }
